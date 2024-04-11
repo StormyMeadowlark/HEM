db279724c9131d06273be46a4b738d33e0c2acd2
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-04-10 22:00:02 -0500

    finished hover state for landing experience. need to change icons to svg files so useStates works. Created Snap Financing button

--- HEM-FrontEnd/src/LandingExperience.jsx
+++ HEM-FrontEnd/src/LandingExperience.jsx
@@ -1,23 +1,24 @@
+import useHover from "./assets/ComponentParts/Buttons/useHover"
 export default function LandingExperience() {
+  const [FleetIsHovering, FleetHoverProps] = useHover()
+  const [RetailIsHovering, RetailHoverProps] = useHover()
+  const [DealershipIsHovering, DealershipHoverProps] = useHover()
   return(
     <div>
       <h1>Experience auto repair<br></br>made for<br></br>YOU</h1>
       <div className='bg-hemautoBlack flex text-hemautoWhite flex-1 justify-around'>
-        <div className="relative w-96">
-          <img src="src/assets/Images/SilverOutlinedTintedHexagon.png" alt="Avatar" class="object-cover w-full h-full"></img>
-          <div>
-            <div className="visible hover:collapse absolute w-full text-3xl text-center inset-y-1/2 font-hemautoFontPlay font-extrabold">RETAIL</div>
-            <div className="hide hover:visible absolute w-full text-3xl text-center inset-y-1/2 font-hemautoFontPlay font-extrabold">BOO!</div>
-          </div>
-        </div>
-        <div className="relative w-96">
-          <img src="src/assets/Images/SilverOutlinedTintedHexagon.png" alt="Avatar" class="object-cover w-full h-full"></img>
-          <div className="absolute w-full text-3xl text-center inset-y-1/2 font-hemautoFontPlay font-extrabold">FLEET</div>
-        </div>
-        <div className="relative w-96">
-          <img src="src/assets/Images/SilverOutlinedTintedHexagon.png" alt="Avatar" class="object-cover w-full h-full"></img>
-          <div className="absolute w-full text-3xl text-center inset-y-1/2 font-hemautoFontPlay font-extrabold">DEALERSHIP</div>
-        </div>
+        <button {...RetailHoverProps} className="relative w-96">
+          <img src="src/assets/Images/SilverOutlinedTintedHexagon.png" alt="Silver Outlined Tinted Hexagon" class="object-cover w-full h-full"></img>
+          <div className="absolute w-full text-3xl text-center inset-y-1/2 font-hemautoFontPlay font-extrabold">{RetailIsHovering ? "Retail Info Here" : "RETAIL"}</div>
+        </button>
+        <button {...FleetHoverProps} className="relative w-96">
+          <img src="src/assets/Images/SilverOutlinedTintedHexagon.png" alt="Silver Outlined Tinted Hexagon" class="object-cover w-full h-full"></img>
+          <div className="absolute w-full text-3xl text-center inset-y-1/2 font-hemautoFontPlay font-extrabold">{FleetIsHovering ? "Fleet Info Here" : "FLEET"}</div>
+        </button>
+        <button {...DealershipHoverProps} className="relative w-96">
+          <img src="src/assets/Images/SilverOutlinedTintedHexagon.png" alt="Silver Outlined Tinted Hexagon" class="object-cover w-full h-full"></img>
+          <div className="absolute w-full text-3xl text-center inset-y-1/2 font-hemautoFontPlay font-extrabold">{DealershipIsHovering ? "Dealership Info Here" : "DEALERSHIP"}</div>
+        </button>
       </div>
     </div>
   )
--- HEM-FrontEnd/src/TopHeader.jsx
+++ HEM-FrontEnd/src/TopHeader.jsx
@@ -1,123 +1,125 @@
 export default function TopHeader() {
-    return (
-      <div className="bg-gradient-to-b from-hemautoBlack via-hemautoGray to-hemautoBlack shadow-inner shadow-hemautoGray px-8 py-6">
-        <ul className="flex flex-row items-center justify-between text-hemautoWhite px-5">
-          <li className="group relative dropdown font-hemautoFontRoadRage text-lg tracking-wide hover:text-hemautoGreen">
-            <a className="text-3xl">FOLLOW @HEMautomotive</a>
-            <div className="w-72 group-hover:flex flex-row dropdown-menu absolute justify-evenly hidden bg-hemautoMediumGray py-2 rounded-bl-lg rounded-tr-lg shadow-inner shadow-hemautoWhite">
-              <a
-                className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
-                alt="Facebook Icon"
-                href="https://www.facebook.com/hemautomotive">
-                <img src="src/assets/Images/Icons/FacebookIcon.png"></img>
-              </a>
-              <a
-                className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
-                alt="Instagram Icon"
-                href="https://www.instagram.com/hemautomotive"
-              >
-                <img src="src/assets/Images/Icons/InstagramIcon.png"></img>
-              </a>
-              <a
-                className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
-                alt="X Icon"
-                href="https://www.x.com/hemautomotive"
-              >
-                <img src="src/assets/Images/Icons/TwitterIcon.png"></img>
-              </a>
-              <a
-                className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
-                alt="youtube Icon"
-                href="https://www.youtube.com/hemautomotive"
-              >
-                <img src="src/assets/Images/Icons/YouTubeIcon.png"></img>
-              </a>
-              <a
-                className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
-                alt="Tiktok Icon"
-                href="https://www.tiktok.com/hemautomotive"
-              >
-                <img src="src/assets/Images/Icons/TikTokIcon.png"></img>
-              </a>
-            </div>
-          </li>
-          <li className="inline-flex items-center text-lg">
-            <img className="max-h-10" src="src/assets/Images/Icons/HoursIcon.png" alt="Clock Icon"></img>
-            <h2 className="px-2 font-bold font-hemautoFontPlay text-balance text-xl">
-              Monday to Friday 8:00am - 4:00pm
-            </h2>
-          </li>
-          <li className="inline-flex items-center text-lg">
-            <img className="max-h-10" src="src/assets/Images/Icons/PhoneIcon.png" alt="Phone Icon"></img>
-            <address>
-              <a href="tel:7852203723">
-                <h2 className="px-2 font-bold font-hemautoFontPlay text-xl not-italic text-balance">
-                  &#40;785&#41; 730-2900
-                </h2>
-              </a>
-            </address>
-          </li>
-          <li className="inline-flex items-center">
-            <img className="max-h-10" src="src/assets/Images/Icons/LocationIcon.png" alt="Location Icon"></img>
-            <address>
-              <a href="address:315 SW 32nd Ter. Topeka, KS 66611">
-                <h2 className="px-2 font-bold font-hemautoFontPlay text-xl not-italic text-balance">
-                  315 SW 32nd Terrace,  Topeka, KS 66611
-                </h2>
-              </a>
-            </address>
-          </li>
-          <li className="group relative dropdown font-hemautoFontRoadRage text-3xl tracking-wide hover:text-hemautoGreen">
-            <a>LEAVE US A REVIEW</a>
-            <div className="w-72 group-hover:flex flex-row dropdown-menu absolute justify-evenly hidden bg-hemautoMediumGray py-2 rounded-br-lg rounded-tl-lg shadow-inner shadow-hemautoWhite">
-              <a
-                className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
-                alt="BBB"
-                href="https://www.bbb.org"
-              >
-                <img src="src/assets/Images/Icons/BBBIcon.png"></img>
-              </a>
-              <a
-                className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
-                alt="Google Icon"
-                href="https://www.google.com"
-              >
-                <img src="src/assets/Images/Icons/GoogleIcon.png"></img>
-              </a>
-              <a
-                className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
-                alt="Yelp Icon"
-                href="https://www.yelp.com"
-              >
-                <img src="src/assets/Images/Icons/YelpIcon.png"></img>
-              </a>
-              <a
-                className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
-                alt="Bing Icon"
-                href="https://www.bing.com"
-              >
-                <img src="src/assets/Images/Icons/BingIcon.png"></img>
-              </a>
-              <a
-                className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
-                alt="Yahoo Icon"
-                href="https://www.yahoo.com"
-              >
-                <img src="src/assets/Images/Icons/YahooIcon.png"></img>
-              </a>
-              <a
-                className="text-hemautoGreen px-2 hover:text-hemautoLightGreen"
-                alt="CarFax Icon"
-                href="https://www.carfax.com"
-              >
-                <img src="src/assets/Images/Icons/CarFaxIcon.png"></img>
-              </a>
-            </div>
-          </li>
-          <li>
-              <button>Financing Information</button>
-          </li>
-        </ul>
-      </div>
-    );
+  return (
+    <div className="bg-gradient-to-b from-hemautoBlack via-hemautoGray to-hemautoBlack shadow-inner shadow-hemautoGray px-8 py-6">
+      <ul className="flex flex-row items-center justify-between text-hemautoWhite px-5">
+        <li className="group relative dropdown font-hemautoFontRoadRage text-lg tracking-wide hover:text-hemautoGreen">
+          <a className="text-3xl">FOLLOW @HEMautomotive</a>
+          <div className="w-72 group-hover:flex flex-row dropdown-menu absolute justify-evenly hidden bg-hemautoMediumGray py-2 rounded-bl-lg rounded-tr-lg shadow-inner shadow-hemautoWhite">
+            <a
+              className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
+              alt="Facebook Icon"
+              href="https://www.facebook.com/hemautomotive">
+              <img src="src/assets/Images/Icons/FacebookIcon.png"></img>
+            </a>
+            <a
+              className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
+              alt="Instagram Icon"
+              href="https://www.instagram.com/hemautomotive"
+            >
+              <img src="src/assets/Images/Icons/InstagramIcon.png"></img>
+            </a>
+            <a
+              className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
+              alt="X Icon"
+              href="https://www.x.com/hemautomotive"
+            >
+              <img src="src/assets/Images/Icons/TwitterIcon.png"></img>
+            </a>
+            <a
+              className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
+              alt="youtube Icon"
+              href="https://www.youtube.com/hemautomotive"
+            >
+              <img src="src/assets/Images/Icons/YouTubeIcon.png"></img>
+            </a>
+            <a
+              className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
+              alt="Tiktok Icon"
+              href="https://www.tiktok.com/hemautomotive"
+            >
+              <img src="src/assets/Images/Icons/TikTokIcon.png"></img>
+            </a>
+          </div>
+        </li>
+        <li className="inline-flex items-center text-lg">
+          <img className="max-h-10" src="src/assets/Images/Icons/HoursIcon.png" alt="Clock Icon"></img>
+          <h2 className="px-2 font-bold font-hemautoFontPlay text-balance text-xl">
+            Monday to Friday 8:00am - 4:00pm
+          </h2>
+        </li>
+        <li className="inline-flex items-center text-lg">
+          <img className="max-h-10" src="src/assets/Images/Icons/PhoneIcon.png" alt="Phone Icon"></img>
+          <address>
+            <a href="tel:7852203723">
+              <h2 className="px-2 font-bold font-hemautoFontPlay text-xl not-italic text-balance">
+                &#40;785&#41; 730-2900
+              </h2>
+            </a>
+          </address>
+        </li>
+        <li className="inline-flex items-center">
+          <img className="max-h-10" src="src/assets/Images/Icons/LocationIcon.png" alt="Location Icon"></img>
+          <address>
+            <a href="address:315 SW 32nd Ter. Topeka, KS 66611">
+              <h2 className="px-2 font-bold font-hemautoFontPlay text-xl not-italic text-balance">
+                315 SW 32nd Terrace,  Topeka, KS 66611
+              </h2>
+            </a>
+          </address>
+        </li>
+        <li className="group relative dropdown font-hemautoFontRoadRage text-3xl tracking-wide hover:text-hemautoGreen">
+          <a>LEAVE US A REVIEW</a>
+          <div className="w-72 group-hover:flex flex-row dropdown-menu absolute justify-evenly hidden bg-hemautoMediumGray py-2 rounded-br-lg rounded-tl-lg shadow-inner shadow-hemautoWhite">
+            <a
+              className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
+              alt="BBB"
+              href="https://www.bbb.org"
+            >
+              <img src="src/assets/Images/Icons/BBBIcon.png"></img>
+            </a>
+            <a
+              className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
+              alt="Google Icon"
+              href="https://www.google.com"
+            >
+              <img src="src/assets/Images/Icons/GoogleIcon.png"></img>
+            </a>
+            <a
+              className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
+              alt="Yelp Icon"
+              href="https://www.yelp.com"
+            >
+              <img src="src/assets/Images/Icons/YelpIcon.png"></img>
+            </a>
+            <a
+              className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
+              alt="Bing Icon"
+              href="https://www.bing.com"
+            >
+              <img src="src/assets/Images/Icons/BingIcon.png"></img>
+            </a>
+            <a
+              className="text-hemautoGreen pl-2 hover:text-hemautoLightGreen"
+              alt="Yahoo Icon"
+              href="https://www.yahoo.com"
+            >
+              <img src="src/assets/Images/Icons/YahooIcon.png"></img>
+            </a>
+            <a
+              className="text-hemautoGreen px-2 hover:text-hemautoLightGreen"
+              alt="CarFax Icon"
+              href="https://www.carfax.com"
+            >
+              <img src="src/assets/Images/Icons/CarFaxIcon.png"></img>
+            </a>
+          </div>
+        </li>
+        <li>
+            <button>
+              <img className="h-[70px]" src="src/assets/Images/Icons/SnapFinancingIcon.png" alt="Snap Financing Logo" href="https://www.snapfinancing.com"></img>
+            </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
