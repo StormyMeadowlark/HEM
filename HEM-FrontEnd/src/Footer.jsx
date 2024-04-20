e879fff449be9ab0f41d254006d61a7b6fcbfd96
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-04-19 23:05:48 -0500

    fixed footer image

--- HEM-FrontEnd/src/Footer.jsx
+++ HEM-FrontEnd/src/Footer.jsx
@@ -1,6 +1,10 @@
 export default function Footer() {
   return (
-    <div className="bg-FooterBackground bg-cover bg-top h-[550px] mt-[-2px]">
+    <div className="bg-FooterBackground bg-cover bg-top h-[1000px] text-hemautoLightGreen">
+      <div>Footer top</div>
+        <>Services listed again as links</>
+        <></>
+      <div>Footer bottom</div>
     </div>
   );
 }
